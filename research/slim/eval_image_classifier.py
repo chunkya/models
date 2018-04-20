f1b3a54dd7d5b9e459faf1c516e72df8b31f195c
booneng <ohbooneng@gmail.com>
2018-04-20 11:18:08 +0800

    print other metrics

--- research/slim/eval_image_classifier.py
+++ research/slim/eval_image_classifier.py
@@ -214,16 +214,14 @@ def main(_):
                                                    dataset.num_classes - FLAGS.labels_offset),
     })
 
-    # Print the summaries to screen.
-    # for name, value in names_to_values.items():
-    #   summary_name = 'eval/%s' % name
-    #   op = tf.summary.scalar(summary_name, value, collections=[])
-    #   op = tf.Print(op, [value], summary_name)
-    #   tf.add_to_collection(tf.GraphKeys.SUMMARIES, op)
+    Print the summaries to screen.
+    for name, value in names_to_values.items():
+      if name != 'Confusion_matrix':
+        summary_name = 'eval/%s' % name
+        op = tf.summary.scalar(summary_name, value, collections=[])
+        op = tf.Print(op, [value], summary_name)
+        tf.add_to_collection(tf.GraphKeys.SUMMARIES, op)
 
-    # c_matrix = slim.metrics.confusion_matrix(predictions, labels)
-    # op = tf.Print(tf.as_string(c_matrix), [c_matrix], 'confusion_matrix')
-    # tf.add_to_collection(tf.GraphKeys.EVAL_STEP, op)
     # TODO(sguada) use num_epochs=1
     if FLAGS.max_num_batches:
       num_batches = FLAGS.max_num_batches
